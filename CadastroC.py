8515a648b6af97e7f9c3295f0805bbc7179ba936
Gabriel Gregorio <gabrielcosta171@yahoo.com>
2026-05-10 16:29:59 -0300

    Deletar usuário

--- CadastroC.py
+++ CadastroC.py
@@ -9,10 +9,18 @@ from sqlalchemy.orm import Session
 
 cliente_router = APIRouter(prefix="/cadastro ",tags=["cadastro"])
 
-@cliente_router.post("/criar_cadastro" )
+@cliente_router.post("/cadastro" )
 async def cadastro_cliente(cliente_schema : ClienteSchema,session :Session = Depends (pegar_sessao)):
     if True:
         novo_cliente = Cliente(cliente_schema.Nome_do_Cliente,cliente_schema.Email,cliente_schema.Telefone ,cliente_schema.IMEI,cliente_schema.Data_venda,cliente_schema.Data_vencimento)
-        session.add(novo_cliente)
-        session.commit()
         return{"mensagem":"Tudo Certo!"}
+
+@cliente_router.delete("/apagar")
+async def delete_cliente(cliente_schema : ClienteSchema,session :Session = Depends (pegar_sessao)):
+    cliente = session.query(Cliente).filter(Cliente.id ==cliente_schema.id ).first()
+    if not cliente:
+        raise HTTPException(status_code=404, detail="Cliente não encontrado")
+    
+    # Soft Delete: apenas marcamos como inativo
+    cliente.ativo = False 
+    session.commit()
